e395cbf3ed4ead516ec3d47daba88888a769f97c
Katia Hernandez <104397570+KatiHernadez@users.noreply.github.com>
2026-01-22 15:21:51 -0600

    diseño de los detalles para historial
    al aparte de los detalles de cada descarga en el historial se le agrego colores, iconos y se ordeno de una mejor manera para visualizarlo de manera correcta

--- src/app/pages/detalle-descarga/detalle-descarga.component.ts
+++ src/app/pages/detalle-descarga/detalle-descarga.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   standalone: true,
   selector: 'app-detalle-descarga',
+  imports: [ MatIconModule],
   templateUrl: './detalle-descarga.html',
   styleUrls: ['./detalle-descarga.css']
 })
@@ -17,8 +19,8 @@ export class DetalleDescarga implements OnInit {
     mes: 'Enero 2026',
     estado: 'COMPLETADO_CON_ERRORES',
     ruta: 'C:/Recibos/Calkini/Enero',
-    inicio: '18/01/2026 10:30',
-    fin: '18/01/2026 10:35',
+    inicio: '18/01/2026 10:30 AM',
+    fin: '18/01/2026 10:35 AM',
     tiempo: '5 minutos',
     tamanio: '5 GB',
     folios: 1500,
