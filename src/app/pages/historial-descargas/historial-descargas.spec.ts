e0187c036df15d51c0e5c0748cd0a80373136428
Maria Ignot <maria.ignot@campeche.gob.mx>
2026-01-30 14:30:55 -0600

    99
    999

--- src/app/pages/historial-descargas/historial-descargas.spec.ts
+++ src/app/pages/historial-descargas/historial-descargas.spec.ts
@@ -1,22 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HistorialDescargas } from './historial-descargas';
+import { HistorialDescargasComponent } from './historial-descargas.component';
 
 describe('HistorialDescargas', () => {
-  let component: HistorialDescargas;
-  let fixture: ComponentFixture<HistorialDescargas>;
+  let component: HistorialDescargasComponent;
+  let fixture: ComponentFixture<HistorialDescargasComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HistorialDescargas]
+      imports: [HistorialDescargasComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HistorialDescargas);
+    fixture = TestBed.createComponent(HistorialDescargasComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
-
+    
   it('should create', () => {
     expect(component).toBeTruthy();
   });
