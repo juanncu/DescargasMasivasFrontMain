35c593cbf8a07336b9f2db20bac3068e613a01a5
Katia Hernandez <104397570+KatiHernadez@users.noreply.github.com>
2026-02-12 14:30:02 -0600

    Reapply "diseño hora"
    This reverts commit 92de3b92d44e6eef58f15560d3e4c9582e0a3362.

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,44 +1,67 @@
-import { Component, OnDestroy } from '@angular/core'; // Añadimos OnDestroy por buena práctica
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common'; // Necesario para usar [ngClass] en standalone
-import { UiService } from '../../services/ui.services'; // Revisa si lleva 's' o no
+import { CommonModule } from '@angular/common';
+import { UiService } from '../../services/ui.services';
+
 import { Subscription } from 'rxjs';
+import{ interval } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.html',
   styleUrls: ['./sidebar.css'],
   standalone: true,
-  imports: [MatIconModule, RouterLink, RouterLinkActive, CommonModule] // Agregamos CommonModule
+  imports: [MatIconModule, RouterLink, RouterLinkActive, CommonModule]
 })
-export class SidebarComponent implements OnDestroy {
-  // 1. Declaramos la propiedad fuera del constructor
-  public isSidebarOpen = false;
-  private sub: Subscription;
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  constructor(private router: Router, private uiService: UiService) {
-    // 2. Nos suscribimos correctamente
+  public isSidebarOpen = false;
+  private sub!: Subscription;
+
+  // Fecha y hora
+  horaActual: Date = new Date();
+  private intervaloHora: any;
+
+  constructor(private router: Router, private uiService: UiService) {}
+
+  // inicia el componente
+  ngOnInit(): void {
+
+    // sidebar open/close
     this.sub = this.uiService.sidebarOpen$.subscribe(status => {
       this.isSidebarOpen = status;
+    }); 
+    this.intervaloHora = interval(1000).subscribe(() => {
+      this.horaActual = new Date();
     });
+
+    // reloj
+    this.intervaloHora = setInterval(() => {
+      this.horaActual = new Date();
+    }, 1000);
   }
 
-  // 3. Importante: Limpiar la suscripción para evitar fugas de memoria
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     if (this.sub) {
       this.sub.unsubscribe();
     }
+
+    if (this.intervaloHora) {
+      this.intervaloHora.unsubscribe();
+    }
   }
 
-  // sidebar.ts
-toggleMenu() {
-  this.uiService.toggleSidebar(); // Esto cerrará el menú al presionar la X
-}
+  toggleMenu(): void {
+    this.uiService.toggleSidebar();
+  }
 
-  cerrarSesion() {
+  cerrarSesion(): void {
     if (confirm('¿Estás seguro de que deseas cerrar sesión?')) {
       this.router.navigate(['/login']);
     }
   }
+
 }
